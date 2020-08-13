52b7eb1c9fa8ee27dad49c5b322ce002c9dcb86e
Madeline McIntosh <mmcintosh2k14@gmail.com>
2020-08-13 14:46:53 -0400

    adds .env.development file

--- graphcms-gatsby-app/gatsby-config.js
+++ graphcms-gatsby-app/gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
+});
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -27,6 +31,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `GraphCMS`,
+        fieldName: `graphCMS`,
+        url: `${process.env.API_URL}`
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
